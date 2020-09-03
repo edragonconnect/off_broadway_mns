57d7ff837164c7def7a5e495346dd5ffd97c11bc
feng19 <feng_19@foxmail.com>
2020-09-03 15:23:07 +0800

    fix(code): remove Receiver

--- lib/off_broadway/mns/producer.ex
+++ lib/off_broadway/mns/producer.ex
@@ -9,20 +9,19 @@ defmodule OffBroadway.MNS.Producer do
 
   alias Broadway.{Message, Acknowledger, Producer}
   alias ExAliyun.MNS
-  alias OffBroadway.MNS.Receiver
 
   @behaviour Acknowledger
   @behaviour Producer
   @max_batch_size 16
   @queue_url_prefix "queues/"
-  @default_receive_opts [number: @max_batch_size, wait_time_seconds: 30]
-  @default_retry_receive_message_interval 1_000
+  @default_receive_opts [number: @max_batch_size]
+  @default_retry_receive_message_interval 5_000
 
   @impl true
   def init(opts) do
     {gen_stage_opts, opts} = Keyword.split(opts, [:buffer_size, :buffer_keep])
 
-    {retry_receive_message_interval, opts} =
+    {receive_interval, opts} =
       Keyword.pop(opts, :retry_receive_message_interval, @default_retry_receive_message_interval)
 
     {queue, opts} =
@@ -35,13 +34,11 @@ defmodule OffBroadway.MNS.Producer do
     prefetch_count = Keyword.fetch!(receive_opts, :number)
     options = producer_options(gen_stage_opts, prefetch_count)
 
-    {:ok, receiver} = Receiver.start_link(queue, receive_opts, retry_receive_message_interval)
-
     state = %{
       queue: queue,
       receive_opts: receive_opts,
-      retry_receive_message_interval: retry_receive_message_interval,
-      receiver: receiver,
+      receive_interval: receive_interval,
+      receive_timer: nil,
       opts: opts,
       demand: 0
     }
@@ -50,51 +47,84 @@ defmodule OffBroadway.MNS.Producer do
   end
 
   @impl true
-  def handle_demand(incoming_demand, state = %{demand: old_demand}) do
-    demand = old_demand + incoming_demand
-
-    if old_demand <= 0 and demand >= 0 do
-      Receiver.start_receive(state.receiver)
-    end
-
-    {:noreply, [], %{state | demand: demand}}
+  def handle_demand(incoming_demand, %{demand: demand} = state) do
+    handle_receive_messages(%{state | demand: demand + incoming_demand})
   end
 
   @impl true
-  def handle_info({:receive, messages}, state = %{queue: queue, demand: old_demand}) do
-    messages =
-      Enum.map(
-        messages,
-        fn message ->
-          {data, metadata} =
-            Map.pop_lazy(
-              message,
-              "MessageBody",
-              fn ->
-                raise KeyError, key: "MessageBody", term: message
-              end
-            )
+  def handle_info(:receive_messages, %{receive_timer: nil} = state) do
+    {:noreply, [], state}
+  end
 
-          %Message{
-            data: data,
-            metadata: metadata,
-            acknowledger: {__MODULE__, _ack_ref = queue, []}
-          }
-        end
-      )
-
-    demand = old_demand - length(messages)
-
-    if old_demand >= -16 && demand <= -16 do
-      Receiver.stop_receive(state.receiver)
-    end
-
-    {:noreply, messages, %{state | demand: demand}}
+  def handle_info(:receive_messages, state) do
+    handle_receive_messages(%{state | receive_timer: nil})
   end
 
   def handle_info(info, state) do
     Logger.warn("unsupported message: " <> inspect(info))
     {:noreply, [], state}
+  end
+
+  @impl Producer
+  def prepare_for_draining(%{receive_timer: receive_timer} = state) do
+    receive_timer && Process.cancel_timer(receive_timer)
+    {:noreply, [], %{state | receive_timer: nil}}
+  end
+
+  defp handle_receive_messages(%{receive_timer: nil, demand: demand} = state) when demand > 0 do
+    messages = receive_messages_from_mns(state.queue, state.receive_opts)
+    new_demand = demand - length(messages)
+
+    receive_timer =
+      case messages do
+        [] -> schedule_receive_messages(state.receive_interval)
+        _ when new_demand <= 0 -> nil
+        _ -> schedule_receive_messages(0)
+      end
+
+    {:noreply, messages, %{state | demand: new_demand, receive_timer: receive_timer}}
+  end
+
+  defp handle_receive_messages(state) do
+    {:noreply, [], state}
+  end
+
+  defp receive_messages_from_mns(queue, opts) do
+    case MNS.receive_message(queue, opts) do
+      {:ok, %{body: %{"Messages" => messages}}} ->
+        # Logger.info("receive message get:" <> inspect(messages))
+        Enum.map(messages, &transform(&1, queue))
+
+      {:error, %{body: %{"Error" => %{"Code" => "MessageNotExist"}}}} ->
+        []
+
+      error ->
+        Logger.warn("receive message error:" <> inspect(error))
+
+        []
+    end
+  end
+
+  @compile {:inline, transform: 2}
+  defp transform(message, queue) do
+    {data, metadata} =
+      Map.pop_lazy(
+        message,
+        "MessageBody",
+        fn ->
+          raise KeyError, key: "MessageBody", term: message
+        end
+      )
+
+    %Message{
+      data: data,
+      metadata: metadata,
+      acknowledger: {__MODULE__, _ack_ref = queue, []}
+    }
+  end
+
+  defp schedule_receive_messages(interval) do
+    Process.send_after(self(), :receive_messages, interval)
   end
 
   @impl Acknowledger
